a3c3df396867031fc1b6bcb5b184dd53bf4d9085
Halimo <mohamedhalimo55j@gmail.com>
2026-08-19 16:31:47 +0300

    models

---
+++ src/models/InterestRequest.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+export enum RequestStatus {
+    PENDING = "pending",
+    ACCEPTED = "accepted",
+    DECLINED = "declined",
+};
+
+const RequestSchema = new mongoose.Schema({
+    listingId: {
+        type: String, 
+        required: true,
+    },
+    seekerId: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: Object.values(RequestStatus),
+        default: RequestStatus.PENDING,
+    },
+});
+
+export const ListingRequest = mongoose.model("ListingRequest", RequestSchema);
