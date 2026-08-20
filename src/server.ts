64dd5e764833249defedf4da8afc6426b0847e99
Halimo <mohamedhalimo55j@gmail.com>
2026-08-20 15:31:23 +0300

    server.ts init

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,16 @@
-console.log("It Works!");
+import "dotenv/config";
+import express from "express";
+import cookieParser from "cookie-parser";
+import { connectDB } from "./config/db";
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieParser());
+
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
