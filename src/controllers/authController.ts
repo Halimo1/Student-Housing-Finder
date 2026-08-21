908fd54152b85ce60548d8cbaf190fd83f428ebe
Halimo <mohamedhalimo55j@gmail.com>
2026-08-22 01:09:55 +0300

    test

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import bcrypt from 'bcryptjs';
+import bcrypt from 'bcrypt';
 import {generateToken} from '../utils/jwt'
 import {User} from '../models/User';
 
